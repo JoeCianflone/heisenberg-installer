4b56dac2c5fe1fe7e7cbfd06aab4879b52d4de8d
Joe Cianflone <joe@cianflone.co>
2016-03-30 17:35:31 -0400

    adds editorconfig file, updates formatting and fixes bug with str_replace

--- src/CleanupCommand.php
+++ src/CleanupCommand.php
@@ -10,56 +10,31 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class CleanupCommand extends Command
 {
-
-    /**
-     * Console command configuration, this is where the command arguments
-     * get set up and what the help menu pull from when it needs info for
-     * about the commands
-     *
-     * @return null
-     */
     protected function configure()
     {
-        $this->setName('cleanup')
-             ->setDescription('Removes any cruft left by the installer');
+      $this->setName('cleanup')
+           ->setDescription('Removes any cruft left by the installer');
     }
 
-    /**
-     * This is the main entry point for the command. When you run it, this
-     * is what it fires
-     *
-     * @param  Symfony\Component\Console\Input\InputInterface  $input
-     * @param  Symfony\Component\Console\Input\OutputInterface $output
-     *
-     * @return null
-     */
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $filesystem = new Filesystem(new Adapter(getcwd().'/'));
+   protected function execute(InputInterface $input, OutputInterface $output)
+   {
+      $filesystem = new Filesystem(new Adapter(getcwd().'/'));
 
-        $output->writeln('<info>Checking for any leftover files...</info>');
-        $currentFiles = $filesystem->listContents();
-        $this->cleanUp($currentFiles, $filesystem);
+      $output->writeln('<info>Checking for any leftover files...</info>');
+      $currentFiles = $filesystem->listContents();
+      $this->cleanUp($currentFiles, $filesystem);
 
-        $output->writeln('<info>So fresh, so clean.</info>');
-    }
+      $output->writeln('<info>So fresh, so clean.</info>');
+   }
 
-    /**
-     * Seeks out any temp folders that may have not been deleted due to
-     * an error durning a prior install.
-     *
-     * @param League\Flysystem\Filesystem $currentFiles contents of the current directory
-     *
-     * @return $this
-     */
-    protected function cleanUp($currentFiles, $filesystem)
-    {
-        foreach ($currentFiles as $file) {
-            if ($file["type"] === "dir" && strpos($file['path'], "mrwhite_") === 0) {
-                $filesystem->deleteDir($file['path']);
-            }
-        }
+   protected function cleanUp($currentFiles, $filesystem)
+   {
+      foreach ($currentFiles as $file) {
+         if ($file["type"] === "dir" && strpos($file['path'], "mrwhite_") === 0) {
+            $filesystem->deleteDir($file['path']);
+         }
+      }
 
-        return $this;
-    }
+      return $this;
+   }
 }
--- src/InstallCommand.php
+++ src/InstallCommand.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Heisenberg\Installer\Console;
 
 use ZipArchive;
@@ -16,155 +15,160 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class InstallCommand extends Command
 {
-    private $repoPath = 'https://github.com/JoeCianflone/heisenberg-toolkit/archive/master.zip';
-    private $tempFolder;
-    private $tempZipFile;
+   private $repoPath = 'https://github.com/JoeCianflone/heisenberg-toolkit/archive/master.zip';
+   private $tempFolder;
+   private $tempZipFile;
 
-    private $folders = [
-        'src',
-        'gulp',
-    ];
+   private $folders = [
+      'src',
+      'gulp',
+   ];
 
-    private $files = [
-        '.bowerrc',
-        '.editorconfig',
-        'bower.json',
-        'gulpfile.js',
-        'package.json',
-        '.gitignore',
-        'gulp/config.js',
-        'heisenberg.html',
-    ];
+   private $files = [
+      '.bowerrc',
+      '.editorconfig',
+      'bower.json',
+      'gulpfile.js',
+      'package.json',
+      '.gitignore',
+      'gulp/config.js',
+      'heisenberg.html',
+   ];
 
-    private $srcPath;
-    private $compiledPath;
+   private $srcPath;
+   private $compiledPath;
 
-    protected function configure()
-    {
-        $this->setName('install')
-             ->setDescription('Installs heisenberg to the current directory.')
-             ->addOption("src", "source files", InputOption::VALUE_OPTIONAL, "where to place src files")
-             ->addOption("dest", "compiled src files", InputOption::VALUE_OPTIONAL, "where heisenberg compiles files to")
-             ->addOption("deps", null, InputOption::VALUE_NONE, "If set this will install bower and npm components");
-    }
+   protected function configure()
+   {
+      $this->setName('install')
+           ->setDescription('Installs heisenberg to the current directory.')
+           ->addOption("src", "source files", InputOption::VALUE_OPTIONAL, "where to place src files")
+           ->addOption("dest", "compiled src files", InputOption::VALUE_OPTIONAL, "where heisenberg compiles files to")
+           ->addOption("deps", null, InputOption::VALUE_NONE, "If set this will install bower and npm components");
+   }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $this->filesystem    = new Filesystem(new Adapter(getcwd().'/'));
-        $this->srcPath       = $input->getOption("src")  ?: "src";
-        $this->compiledPath  = $input->getOption("dest") ?: "assets";
-        $this->tempFolder    = 'mrwhite_'.md5(time().uniqid());
-        $this->tempZipFile   = $this->tempFolder . '/heisenberg_'.md5(time().uniqid()) . ".zip";
+   protected function execute(InputInterface $input, OutputInterface $output)
+   {
+      $this->filesystem    = new Filesystem(new Adapter(getcwd().'/'));
+      $this->srcPath       = $input->getOption("src")  ?: "src";
+      $this->compiledPath  = $input->getOption("dest") ?: "assets";
+      $this->tempFolder    = 'mrwhite_'.md5(time().uniqid());
+      $this->tempZipFile   = $this->tempFolder . '/heisenberg_'.md5(time().uniqid()) . ".zip";
 
+      $output->writeln('<info>Installing Heisenberg...</info>');
+      $this->download()
+           ->extract()
+           ->move()
+           ->cleanup()
+           ->dependencies($input->getOption('deps'), $output);
 
-        $output->writeln('<info>Installing Heisenberg...</info>');
-        $this->download()
-             ->extract()
-             ->move()
-             ->cleanup()
-             ->dependencies($input->getOption('deps'), $output);
+      $output->writeln('<info>Heisenberg Installed. Now...</info>');
+      $output->writeln('<info>Say. My. Name.</info>');
+   }
 
-        $output->writeln('<info>Heisenberg Installed. Now...</info>');
-        $output->writeln('<info>Say. My. Name.</info>');
-    }
+   protected function download()
+   {
+      $response = (new Client)->get($this->repoPath);
 
-    protected function download()
-    {
-        $response = (new Client)->get($this->repoPath);
+      $this->filesystem->createDir($this->tempFolder);
+      $this->filesystem->write($this->tempZipFile, $response->getBody());
 
-        $this->filesystem->createDir($this->tempFolder);
-        $this->filesystem->write($this->tempZipFile, $response->getBody());
+      return $this;
+   }
 
-        return $this;
-    }
+   protected function extract()
+   {
+      $archive = new ZipArchive;
 
-    protected function extract()
-    {
-        $archive = new ZipArchive;
+      $archive->open($this->tempZipFile);
+      $archive->extractTo($this->tempFolder);
+      $archive->close();
 
-        $archive->open($this->tempZipFile);
-        $archive->extractTo($this->tempFolder);
-        $archive->close();
+      return $this;
+   }
 
-        return $this;
-    }
+   protected function move()
+   {
+      $dlPath = $this->tempFolder . "/heisenberg-toolkit-master/";
+      $this->moveFolders($dlPath);
+      $this->moveFiles($dlPath);
 
-    protected function move()
-    {
-        $dlPath = $this->tempFolder . "/heisenberg-toolkit-master/";
-        $this->moveFolders($dlPath);
-        $this->moveFiles($dlPath);
+      return $this;
+   }
 
-        return $this;
-    }
+   private function moveFolders($dlPath)
+   {
+      foreach ($this->folders as $folder) {
+         $list = $this->filesystem->listContents($dlPath.$folder, true);
+         $this->copyFiles($list, $dlPath);
+      }
+   }
 
-    private function moveFolders($dlPath)
-    {
-        foreach ($this->folders as $folder) {
-            $list = $this->filesystem->listContents($dlPath.$folder, true);
-            $this->copyFiles($list, $dlPath);
-        }
-    }
+   private function moveFiles($dlPath)
+   {
+      foreach ($this->files as $file) {
+         $fileContent = $this->filesystem->read($dlPath.$file);
+         $fileContent = $this->updatePaths($fileContent);
 
-    private function moveFiles($dlPath)
-    {
-        foreach ($this->files as $file) {
-            $fileContent = $this->filesystem->read($dlPath.$file);
-            $fileContent = $this->updatePaths($fileContent);
+         $this->filesystem->put($file, $fileContent);
+      }
+   }
 
-            $this->filesystem->put($file, $fileContent);
-        }
-    }
+   private function updatePaths($content)
+   {
+      // It's a long story, but yes, you do indeed need to do
+      // two separate transformations here.
+      $content = str_replace("assets/", "{{dest}}", str_replace("src/", "{{source}}", $content));
+      $content = str_replace('{{dest}}', rtrim($this->compiledPath, '/').'/', str_replace('{{source}}', rtrim($this->srcPath,'/').'/', $content));
 
-    private function updatePaths($content)
-    {
-        $content = str_replace('src/', $this->srcPath.'/', $content);
-        $content = str_replace('assets/', $this->compiledPath.'/', $content);
+      return $content;
+   }
 
-        return $content;
-    }
-    private function copyFiles($list, $dlPath) {
-        foreach ($list as $file) {
-            if ($file['type'] === 'file') {
-                $newPath = str_replace($dlPath, "", $file['path']);
-                $newPath = str_replace("src", $this->srcPath, $newPath);
-                $this->filesystem->copy($file['path'], $newPath);
-            }
-        }
-    }
+   private function copyFiles($list, $dlPath)
+   {
+      foreach ($list as $file) {
+         if ($file['type'] === 'file') {
+            $newPath = str_replace($dlPath, "", $file['path']);
+            $newPath = str_replace("src", rtrim($this->srcPath, '/'), $newPath);
+            $this->filesystem->copy($file['path'], $newPath);
+         }
+      }
+   }
 
-    protected function cleanup()
-    {
-        $this->filesystem->deleteDir($this->tempFolder);
-        return $this;
-    }
+   protected function cleanup()
+   {
+      $this->filesystem->deleteDir($this->tempFolder);
 
-    protected function dependencies($installDeps, $output)
-    {
-        if ($installDeps) {
-            $output->writeln("<info>Attempting to install dependencies, this will take a moment...</info>");
-            try {
-                $this->processCLI("npm install");
-                $this->processCLI("bower install");
-                $this->processCLI("gulp compile");
-            } catch(ProcessFailedException $e) {
-                $output->writeln("<error>Error ProcessFailedException".$e->getMessage()."</error>");
-            }
-        }
-        return $this;
-    }
+      return $this;
+   }
 
-    private function processCLI($cmd)
-    {
-        $process = new Process($cmd);
-        $process->setTimeout(3600);
-        $process->run(function ($type, $buffer) {
-            echo $buffer;
-        });
+   protected function dependencies($installDeps, $output)
+   {
+      if ($installDeps) {
+         $output->writeln("<info>Attempting to install dependencies, this will take a moment...</info>");
+         try {
+            $this->processCLI("npm install");
+            $this->processCLI("bower install");
+            $this->processCLI("gulp compile");
+         } catch(ProcessFailedException $e) {
+            $output->writeln("<error>Error ProcessFailedException".$e->getMessage()."</error>");
+         }
+      }
 
-        if (! $process->isSuccessful()) {
-            throw new ProcessFailedException($process);
-        }
-    }
+      return $this;
+   }
+
+   private function processCLI($cmd)
+   {
+      $process = new Process($cmd);
+      $process->setTimeout(3600);
+      $process->run(function ($type, $buffer) {
+         echo $buffer;
+      });
+
+      if (! $process->isSuccessful()) {
+         throw new ProcessFailedException($process);
+      }
+   }
 }
 
